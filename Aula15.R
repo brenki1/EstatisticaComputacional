0489e730d3ec24b67120b2e58a5273b083ae5c5e
brenki1 <pagioliosvaldo@gmail.com>
2024-10-02 18:17:46 -0300

    Aula 15

--- Aula15.R
+++ Aula15.R
@@ -20,3 +20,42 @@ conjunto |>
   count(word, sort = TRUE) |>
   top_n(10)
 
+stopwords_br <- data.frame(word = stopwords("pt"))
+conjunto |>
+  unnest_tokens(output = word, input = texto) |>
+  anti_join(stopwords_br) |>
+  count(word, sort = TRUE) |>
+      top_n(10) |>
+  mutate(word = reorder(word,n)) |>
+  ggplot(aes(x = n, y = word))+
+  geom_col(color = "purple",fill = "#fc03e3") +
+  theme_minimal()
+
+library(janeaustenr)
+
+stopwords_en <- data.frame(word = stopwords("en"))
+livro <- prideprejudice
+conjunto <- data.frame(livro)
+conjunto |>
+  unnest_tokens(output = word, input = livro) |>
+  anti_join(stopwords_en) |>
+  count(word, sort = TRUE) |>
+    top_n(10) |>
+  mutate(word = reorder(word,n)) |>
+  ggplot(aes(x = n,y=word)) +
+  geom_col(color = "yellow", fill = "red") +
+  theme_minimal()
+     
+sentimentos <- get_sentiments("bing")
+library("stringr")
+livro <- data.frame(texto = livro)
+capitulos <- str_detect(livro$texto, "^Chapter \\d+")
+capitulos <- cumsum(capitulos)
+
+livro |>
+  mutate(capitulo = capitulos) |>
+  unnest_tokens(word,texto) |>
+  inner_join(sentimentos) |>
+  count(capitulo,sentiment) |>
+  spread(sentiment,n,fill=0)
+
